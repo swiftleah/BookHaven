4e2608468c464b8f5b0416896582bdd77bcb52d1
swiftleah <leahrosehenry@gmail.com>
2024-05-17 01:37:50 -0700

    Created route for login as well. Looks for username & password (not email) -> if (user) -> generates JWT token and sends it to client -> valid for 1h

--- book-library/routes/auth.js
+++ book-library/routes/auth.js
@@ -9,7 +9,7 @@ const User = require('../models/User');
 
 const router = express.Router();
 
-// User registration - website'/register'
+// User Registration - website'/register'
 router.post('/register', async (req, res) => {
 	try {
 		// Data sent by client to server
@@ -19,7 +19,7 @@ router.post('/register', async (req, res) => {
 		// Check if user is already in db
 		let user = await User.findOne({ email });
 		if (user) {
-			return res.status(400).json({message: 'User already exists' });
+			return res.status(400).json({ message: 'User already exists' });
 		}
 
 		// Generates salt to be combined with password before hashing
@@ -36,9 +36,53 @@ router.post('/register', async (req, res) => {
 		// Save new user info with the hashed password above
 		await user.save();
 
-		res.status(201).json({message: 'User registered successfully' });
+		res.status(201).json({ message: 'User registered successfully' });
 	} catch (err) {
 		console.error(err.message);
+		// Sets HTTP status to 500 & sends str 'Server Error' as response body
 		res.status(500).send('Server Error');
 	}
 });
+
+// User Login
+router.post('/login', async (req, res) => {
+	try {
+		const { username, password } = req.body;
+
+		// Looking if user exists in db by username
+		let user = await User.findOne({ username });
+		if (!user) {
+			return res.status(400).json({ message: 'Invalid credentials' });
+		}
+
+		// Comparing password received to hashed password in db
+		const isMatch = await bcrypt.compare(password, user.password);
+		if (!isMatch) {
+			return res.status(400).json({ message: 'Invalid credentials' });
+		}
+
+		// Generate JWT token
+		// Payload for token - consists of user id (claim)
+		const payload {
+			user: {
+				id: user.id
+			}
+		};
+
+		// Token signature
+		// Encoded payload & secret are wrapped - sent back to client & valid for 1h
+		// If error occurs, will be passed in err
+		jwt.sign(payload, 'jwtSecret', { expiresIn: 3600 }, (err, token) => {
+			if (err) throw err;
+			// Sends JSON obj to client to be used
+			res.json({ token });
+		});
+	// Catch error and log error to console
+	} catch (err) {
+		console.error(err.message);
+		// Sets HTTP status & sends str as response body
+		res.status(500).send('Server Error');
+	}
+});
+
+module.exports = router;
