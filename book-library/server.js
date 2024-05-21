ce2458a0b983e1fc26b6430f67966e3454fac566
swiftleah <leahrosehenry@gmail.com>
2024-05-21 04:38:39 -0700

    Updated routes in server file

--- book-library/server.js
+++ book-library/server.js
@@ -6,7 +6,10 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+
 const authRoutes = require('./routes/auth');
+const bookRoutes = require('./routes/books');
+const noteRoutes = require('./routes/notes');
 
 // Create instance of express framework
 // Where app represents our web application
@@ -14,14 +17,10 @@ const app = express();
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/book-library', {
-	// Uses latest URL parser available to interpret connection string provided (URI)
 	useNewUrlParser: true,
-	// Instructs mongoose to use new Server Discovery and Monitoring engine
 	useUnifiedTopology: true,
 })
-// Handles successful connection
 .then(() => console.log('Connected to MongoDB'))
-// Handles error - failed connection
 .catch(err => console.error('Failed to connect to MongoDB', err));
 
 // Middleware
@@ -29,7 +28,8 @@ app.use(express.json());
 
 // Middleware - applied to requests that start with /api/auth
 app.use('/api/auth', authRoutes);
+app.use('/api/books', bookRoutes);
+app.use('/api/notes', noteRoutes);
 
-// Retrieve PORT number || set PORT to 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
